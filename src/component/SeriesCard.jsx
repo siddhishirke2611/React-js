0479a032e8e950c92b97508ab531c06dff9c5c62
siddhi <siddhishirke.hkb@gmail.com>
2024-10-01 14:28:24 +0530

    css

--- src/component/SeriesCard.jsx
+++ src/component/SeriesCard.jsx
@@ -1,26 +1,34 @@
 import React from 'react'
-
+import styled from 'styled-components';
 const SeriesCard = (props) => {
 
-  //const {id,img_url, name, rating, description, cast, genre, watch_url} = props.curElem
+  const { id, img_url, name, rating, description, cast, genre, watch_url } = props.curElem
   // destructuring props
+
+  const ratingClass = rating >= 8.5 ? "super_hit" : "average";
+
+  const btn_style = {
+    padding: "1.2rem 2.4rem",
+    border: "none",
+    fontSize: "1.6rem",
+    backgroundColor: `${rating >= 8.5 ? "#7dcea0" : "#f7dc6f"}`,
+    color:"var(--btn-color)",
+  };
   return (
     <li className='card'>
       <div>
         <div>
-          <img src={props.curElem.img_url} alt='banner' width="40%" height="40%" />
+          <img src={img_url} alt='banner' width="40%" height="40%" />
         </div>
         <div className='card-content'>
-        <h2>Name : {props.curElem.name} </h2>
-        <h3>Rating: {props.curElem.rating}</h3>
-        <h3>Summery:{props.curElem.description}</h3>
-        <p>Genre: {props.curElem.genre}</p>
-        <p>Cast: {props.curElem.cast}</p>
-        <a href={props.curElem.watch_url} target='_blank'></a>
-        <button style={{padding:"1.2rem 2.4rem", border:"none", fontSize:"1.6rem",
-          backgroundColor:"var(--bnt-hover-bg-color)",
-          color:"var(--bg-color)"
-        }}>Watch now</button>
+          <h2>Name : {name} </h2>
+          <h3>Rating: <span className={`rating ${ratingClass}`}>{rating}</span>
+          </h3>
+          <p>Summery:{description}</p>
+          <p>Genre: {genre}</p>
+          <p>Cast: {cast}</p>
+          <a href={watch_url} target='_blank'></a>
+          <button style={btn_style}>Watch now</button>
         </div>
       </div>
     </li>
